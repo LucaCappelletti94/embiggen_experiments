f799868fd08ac4362b96fdfb7c7b1af6807605fa
Vida Ravanmehr <vidarmehr@gmail.com>
2020-09-22 10:43:08 -0400

    add codes that exist in both skipgrapm and cbow in one python file common_codes.py

---
+++ n_1/common_codes.py
@@ -0,0 +1,126 @@
+import silence_tensorflow.auto # Import needed to avoid TensorFlow warnings and general useless infos.
+from ensmallen_graph import EnsmallenGraph
+from embiggen import Node2VecSequence
+
+from tensorflow.distribute import MirroredStrategy
+from tensorflow.keras.optimizers import Nadam
+from embiggen import SkipGram
+from embiggen import CBOW
+from tensorflow.keras.callbacks import EarlyStopping
+from plot_keras_history import plot_history
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class GrapEmbedding:
+    def __init__(self, edge_path, walk_length, batch_size, iterations, window_size, p, q, delta, patience, embedding_size, negative_samples
+                 , embedding_model, learning_rate,epochs):
+        self.edgelist_path = edge_path
+        self.walk_length = walk_length
+        self.batch_size = batch_size
+        self.iterations = iterations
+        self.window_size = window_size
+        self.p = p
+        self.q = q
+        self.delta = delta
+        self.patience = patience
+        self.embedding_size = embedding_size
+        self.negative_samples = negative_samples
+        self.embedding_model = embedding_model
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+
+
+
+    def read_graph(self):
+        graph = EnsmallenGraph.from_csv(
+        edge_path=self.edgelist_path, #data/ppi/edges.tsv
+        sources_column="subject",
+        destinations_column="object",
+        directed=False,
+        weights_column="weight"
+        )
+        print(graph.report())
+        return graph
+
+    def create_training_validation(self,graph):
+        training, validation = graph.connected_holdout(42, 0.8)
+        assert graph > training
+        assert graph > validation
+        assert (training + validation).contains(graph)
+        assert graph.contains(training + validation)
+        assert not training.overlaps(validation)
+        assert not validation.overlaps(training)
+        return training, validation
+
+
+
+    def train_seq(self,training):
+        training_sequence = Node2VecSequence(
+        training,
+        walk_length=self.walk_length,
+        batch_size=self.batch_size,
+        iterations=self.iterations,
+        window_size=self.window_size,
+        return_weight=1/self.p,
+        explore_weight=1/self.q
+        )
+        return training_sequence
+
+    def valid_seq(self,graph):
+        validation_sequence = Node2VecSequence(
+        graph, # Here we use the entire graph. This will only be used for the early stopping.
+        walk_length=self.walk_length,
+        batch_size=self.batch_size,
+        iterations=self.iterations,
+        window_size=self.window_size,
+        return_weight=1/self.p,
+        explore_weight=1/self.q
+        )
+        return validation_sequence
+
+    def embed_model(self,  training):
+        strategy = MirroredStrategy()
+        if self.embedding_model == "skipgram":
+             with strategy.scope():
+                 model = SkipGram(
+                     vocabulary_size=training.get_nodes_number(),
+                     embedding_size=self.embedding_size,
+                     window_size=self.window_size,
+                     negatives_samples=self.negative_samples,
+                     optimizer=Nadam(learning_rate=self.learning_rate)
+                 )
+        elif self.embedding_model == "cbow":
+            with strategy.scope():
+                model = CBOW(
+                    vocabulary_size=training.get_nodes_number(),
+                    embedding_size=self.embedding_size,
+                    window_size=self.window_size,
+                    negatives_samples=self.negative_samples,
+                    optimizer=Nadam(learning_rate=self.learning_rate)
+                )
+
+        print(model.summary())
+        return model
+
+    def history(self,training_sequence,validation_sequence,model):
+        history = model.fit(
+        training_sequence,
+        steps_per_epoch=training_sequence.steps_per_epoch,
+        validation_data=validation_sequence,
+        validation_steps=validation_sequence.steps_per_epoch,
+        epochs=self.epochs,
+        callbacks=[
+            EarlyStopping(
+                "val_loss",
+                min_delta=self.delta,
+                patience=self.patience,
+                restore_best_weights=True
+            )
+        ]
+        )
+
+        model.save_weights(f"{model.name}_weights.h5")
+        plot_history(history)
+        plt.savefig("history.png")
+        np.save(f"{model.name}_embedding.npy", model.embedding)
--- n_1/skipgram_graph_embedding.py
+++ n_1/skipgram_graph_embedding.py
@@ -10,27 +10,10 @@ from plot_keras_history import plot_history
 import numpy as np
 import matplotlib.pyplot as plt
 
+from common_codes import GrapEmbedding
 
-
-
-
-
-graph = EnsmallenGraph.from_csv(
-    edge_path="data/ppi/edges.tsv",
-    sources_column="subject",
-    destinations_column="object",
-    directed=False,
-    weights_column="weight"
-)
-graph.report()
-training, validation = graph.connected_holdout(42, 0.8)
-assert graph > training
-assert graph > validation
-assert (training + validation).contains(graph)
-assert graph.contains(training + validation)
-assert not training.overlaps(validation)
-assert not validation.overlaps(training)
-
+edge_path="data/ppi/edges.tsv" #modify if needed
+#get_parameters()#TODO read parameters from a json file
 walk_length=100
 batch_size=2**7
 iterations=20
@@ -38,68 +21,21 @@ window_size=4
 p=1.0
 q=1.0
 embedding_size=100
-negatives_samples=30
+negative_samples=30
 patience=5
 delta=0.0001
 epochs=1000
 learning_rate=0.1
+embedding_model = "skipgram"
 
 
+graph_embed = GrapEmbedding(edge_path, walk_length, batch_size, iterations, window_size, p, q, delta, patience, embedding_size,
+                            negative_samples, embedding_model, learning_rate,epochs)
 
-training_sequence = Node2VecSequence(
-    training,
-    walk_length=walk_length,
-    batch_size=batch_size,
-    iterations=iterations,
-    window_size=window_size,
-    return_weight=1/p,
-    explore_weight=1/q
-)
+graph = graph_embed.read_graph()
+training,validation = graph_embed.create_training_validation(graph)
+training_sequence = graph_embed.train_seq(training)
+validation_sequence = graph_embed.valid_seq(graph)
+model = graph_embed.embed_model(training)
+history = graph_embed.history(training_sequence, validation_sequence, model)
 
-validation_sequence = Node2VecSequence(
-    graph, # Here we use the entire graph. This will only be used for the early stopping.
-    walk_length=walk_length,
-    batch_size=batch_size,
-    iterations=iterations,
-    window_size=window_size,
-    return_weight=1/p,
-    explore_weight=1/q
-)
-
-
-
-strategy = MirroredStrategy()
-with strategy.scope():
-    model = SkipGram(
-        vocabulary_size=training.get_nodes_number(),
-        embedding_size=embedding_size,
-        window_size=window_size,
-        negatives_samples=negatives_samples,
-        optimizer=Nadam(learning_rate=learning_rate)
-    )
-
-model.summary()
-
-
-history = model.fit(
-    training_sequence,
-    steps_per_epoch=training_sequence.steps_per_epoch,
-    validation_data=validation_sequence,
-    validation_steps=validation_sequence.steps_per_epoch,
-    epochs=1000,
-    callbacks=[
-        EarlyStopping(
-            "val_loss",
-            min_delta=delta,
-            patience=patience,
-            restore_best_weights=True
-        )
-    ]
-)
-
-model.save_weights(f"{model.name}_weights.h5")
-
-plot_history(history)
-plt.savefig("history.png")
-
-np.save(f"{model.name}_embedding.npy", model.embedding)
