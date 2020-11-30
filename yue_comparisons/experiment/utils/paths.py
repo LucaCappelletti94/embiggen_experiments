82f3c39482b7cc8cfdbec9ed2808fae43f25aa0f
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-11-30 11:28:14 +0100

    Added method to retrieve paths for node label prediction

--- yue_comparisons/experiment/utils/paths.py
+++ yue_comparisons/experiment/utils/paths.py
@@ -20,3 +20,22 @@ def get_link_prediction_paths(root: str = "data") -> List[str]:
         for graph_directory in glob("{}/*".format(root))
         if len(glob("{}/*.tsv".format(graph_directory))) == 2
     ]
+
+
+def get_node_label_paths(root: str = "data") -> List[str]:
+    """Return path to directories of the graph to be used for node label prediction.
+
+    Parameters
+    -----------------------
+    root: str = "data",
+        Directory from where to search for node label prediction data.
+
+    Returns
+    -----------------------
+    List of paths to the directories.
+    """
+    return [
+        graph_directory
+        for graph_directory in glob("{}/*".format(root))
+        if len(glob("{}/*.tsv".format(graph_directory))) > 2
+    ]
