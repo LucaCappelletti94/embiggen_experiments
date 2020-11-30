9fb3872aba9e1b3ee371c14d874acc9a2e764a52
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-11-30 11:23:23 +0100

    Added method to load the link prediction models

---
+++ yue_comparisons/experiment/utils/paths.py
@@ -0,0 +1,22 @@
+from typing import List
+from glob import glob
+import os
+
+
+def get_link_prediction_paths(root: str = "data") -> List[str]:
+    """Return path to directories of the graph to be used for link prediction.
+
+    Parameters
+    -----------------------
+    root: str = "data",
+        Directory from where to search for link prediction data.
+
+    Returns
+    -----------------------
+    List of paths to the directories.
+    """
+    return [
+        graph_directory
+        for graph_directory in glob("{}/*".format(root))
+        if len(glob("{}/*.tsv".format(graph_directory))) == 2
+    ]
