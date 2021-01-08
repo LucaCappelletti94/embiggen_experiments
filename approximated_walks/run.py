227b5f4e47b3484367945e0696d639679348844a
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-01-08 21:36:48 +0100

    Removed mirror strategy

--- approximated_walks/run.py
+++ approximated_walks/run.py
@@ -1,7 +1,4 @@
 from experiment import run
-import tensorflow as tf
 
 if __name__ == "__main__":
-    strategy = tf.distribute.MirroredStrategy()
-    with strategy.scope():
-        run().to_csv("approximated_walks.csv", index=False)
+    run().to_csv("approximated_walks.csv", index=False)
