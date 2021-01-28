11c9281f0182a38405bc44efb81ca5ca50d8d13c
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-01-28 16:32:49 +0100

    Updated configuration for updated ensmallen graph

--- approximated_walks/experiment/run.py
+++ approximated_walks/experiment/run.py
@@ -2,7 +2,8 @@ from typing import Tuple
 
 import pandas as pd
 from embiggen import LinkPredictionTransformer
-from ensmallen_graph import StringPPI, EnsmallenGraph
+from ensmallen_graph import EnsmallenGraph
+from ensmallen_graph.datasets.string import String
 from tqdm.auto import tqdm, trange
 
 from .multi_layer_perceptron import get_multi_layer_perceptron_predictions
@@ -18,7 +19,7 @@ def run(
     random_state: int = 42
 ) -> pd.DataFrame:
     results = []
-    graph = StringPPI()
+    graph = String()
     negative_graph: EnsmallenGraph = graph.sample_negatives(
         graph.get_edges_number(),
         random_state=random_state,
