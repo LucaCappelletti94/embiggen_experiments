9fe8711668ef91d2a62071826c7c34c113b583ab
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-04-04 16:31:41 +0200

    Added script for retrieving CTD

--- node2vec_comparisons/experiment/__init__.py
+++ node2vec_comparisons/experiment/__init__.py
@@ -1,5 +1,8 @@
+"""Module to run experiments comparing different Node2Vec libraries."""
 from .track import track_all_libraries
+from .data_retrieval import retrieve_ctd
 
 __all__ = [
-    "track_all_libraries"
+    "track_all_libraries",
+    "retrieve_ctd"
 ]
---
+++ node2vec_comparisons/experiment/data_retrieval/__init__.py
@@ -0,0 +1,6 @@
+"""Submodule providing methods to retrieve the data used within the experiments."""
+from .ctd import retrieve_ctd
+
+__all__ = [
+    "retrieve_ctd"
+]
